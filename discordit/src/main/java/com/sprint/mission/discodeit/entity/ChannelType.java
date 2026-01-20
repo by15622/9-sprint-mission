fb0331dab81fb427ddf81058e5cf1542a052cb0c
seo hyeon ha <hyeonha1103@gmail.com>
2026-01-20 19:06:49 +0900

    2026-01-20

---
+++ discordit/src/main/java/com/sprint/mission/discodeit/entity/ChannelType.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.io.Serializable;
+
+public enum ChannelType implements Serializable {;
+    private static final long serialVersionUID = 1L;
+}
