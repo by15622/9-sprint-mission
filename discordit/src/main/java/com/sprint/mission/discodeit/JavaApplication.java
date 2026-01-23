b4da07b6a9dc52e1a9ef5904bf8db8a376c2ce02
seo hyeon ha <hyeonha1103@gmail.com>
2026-01-23 21:58:58 +0900

    2026-01-23

--- discordit/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -4,12 +4,16 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
 import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
 import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
 import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
 import com.sprint.mission.discodeit.entity.Message;
@@ -185,12 +189,14 @@ public class JavaApplication {
     public static void main(String[] args) {
 
         // 서비스 초기화
+        UserRepository userRepository = new FileUserRepository();
+        ChannelRepository channelRepository = new FileChannelRepository();
+        MessageRepository messageRepository = new FileMessageRepository();
         UserService userService = new FileUserService();
         ChannelService channelService = new JCFChannelService();
         MessageService messageService = new JCFMessageService();
-        ChannelRepository channelRepository = new FileChannelRepository();
-        UserRepository userRepository = new FileUserRepository();
         MessageService messageService2 = new BasicMessageService(userRepository,channelRepository);
+
 
         //테스트
         userCRUDTest(userService);
