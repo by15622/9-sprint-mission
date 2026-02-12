21a54fa2229384dea6e2ece61f35346a097dfe9f
seo hyeon ha <hyeonha1103@gmail.com>
2026-02-12 10:43:30 +0900

    2026-02-12

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -2,15 +2,16 @@ package com.sprint.mission.discodeit.controller;
 
 
 import com.sprint.mission.discodeit.dto.MessageCreateRequest;
+import com.sprint.mission.discodeit.dto.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -23,5 +24,25 @@ public class MessageController {
     public ResponseEntity<Message> send(@RequestBody MessageCreateRequest request) {
         Message message = messageService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(message);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Message> update(
+            @PathVariable UUID id,
+            @RequestBody MessageUpdateRequest request) {
+        Message updatedMessage = messageService.update(id, request);
+        return ResponseEntity.ok(updatedMessage);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        messageService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/channel/{channelId}")
+    public ResponseEntity<List<Message>> findAllByChannelId(@PathVariable UUID channelId) {
+        List<Message> messages = messageService.findAllByChannelId(channelId);
+        return ResponseEntity.ok(messages);
     }
 }
