9f94a7ba54c50e900bf1dac4aaf176aad41e16df
seo hyeon ha <hyeonha1103@gmail.com>
2026-04-09 16:34:42 +0900

    fix: 테스트 클래스에 ActiveProfiles 적용 (DB 에러 해결)

--- discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
@@ -15,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(properties = "discodeit.storage.type=s3")
 public class S3BinaryContentStorageTest {
 
