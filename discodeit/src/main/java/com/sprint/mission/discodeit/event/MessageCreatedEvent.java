f996dfee948c361ba3bcfcd77d412ff4f49b6c7f
seo hyeon ha <hyeonha1103@gmail.com>
2026-06-08 10:29:54 +0900

    feat: Kafka, Redis 캐시 도입 및 멘토 피ë님백 반영

--- discodeit/src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
@@ -3,6 +3,6 @@ package com.sprint.mission.discodeit.event;
 import com.sprint.mission.discodeit.dto.data.MessageDto;
 
 public record MessageCreatedEvent(
-    MessageDto message  // 생성된 메시지 전체를 담아요
+    MessageDto message
 ) {
 }
